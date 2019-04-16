b3a962687d4e34dc3123d6e98b6ee0bdf8e073ac
akankshakutal <44019583+akankshakutal@users.noreply.github.com>
2019-04-16 18:56:40 +0530

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      email: "",
+      count: 0
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+      email: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    // fetch(`/api/join_us/${this.state.email}`)
+    //   .then(response => response.text())
+    //   .then(count => {
+    //     this.state.count = count;
+    //     this.state.value = "";
+    //     this.setState(state => state);
+    //   });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+      <div className="flex_container">
+        <div className="container">
+          <h1>JOIN US</h1>
+          <p className="lead">
+            Enter your email to join <strong>{this.state.count}</strong> others
+            on our waitlist. We are 100% not a cult.{" "}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <input
+            type="text"
+            className="form"
+            name="email"
+            placeholder="Enter Your Email"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleSubmit}>Join Now</button>
+        </div>
       </div>
     );
   }
